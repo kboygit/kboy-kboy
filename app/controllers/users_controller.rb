7dec6eeebc314009f44067c18a30f37b2eafd7fd
kboygit <heybisayangdako@gmail.com>
2017-01-01 03:21:01 -0800

    completed user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      flash[:success] = "Success my friend!"
       redirect_to user_url(@user)
+      # redirect_to @user # this work as well #
     else
       render 'new'
     end
