de84a985a10e3ce91652476697be99b4e940a6a6
kboygit <heybisayangdako@gmail.com>
2017-01-01 15:04:17 -0800

    render flash message,flash test,find user,log in form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an erorr message.
+      flash.now[:danger] = 'Kirby says there is a problem with your login.'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+end
