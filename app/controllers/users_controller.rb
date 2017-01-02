6bc78173736b51e0e3c652a31c5e2b389b2e739a
kboygit <heybisayangdako@gmail.com>
2017-01-01 20:58:53 -0800

    implemented user basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Success my friend!"
       redirect_to user_url(@user)
       # redirect_to @user # this work as well #
