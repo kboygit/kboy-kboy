0c4ca385116aa663e7ec5529d15f48548abc221a
kboygit <heybisayangdako@gmail.com>
2017-01-04 22:17:34 -0800

    implement a test for friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       # Create an erorr message.
       flash.now[:danger] = 'Kirby says there is a problem with your login.'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,7 @@ class UsersController < ApplicationController
   # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
+        store_location
         flash[:danger] = "Please log in my friend."
         redirect_to login_url
       end
