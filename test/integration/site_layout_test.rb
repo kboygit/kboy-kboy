db863b7b24c8bdb01aec63e54cbc184a5a11d825
kboygit <heybisayangdako@gmail.com>
2017-01-05 11:23:17 -0800

    created a test for all the layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup               # Added
+    @user = users(:kirby) # for
+  end                     # test
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -15,5 +20,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    assert_select "a[href=?]", login_path
+    log_in_as(@user)
+    get root_path
+  assert_select "a[href=?]", root_path, count: 2
+  assert_select "a[href=?]", help_path
+  assert_select "a[href=?]", about_path
+  assert_select "a[href=?]", contact_path
+  assert_select "a[href=?]", login_path, count: 0
+  assert_select "a[href=?]", users_path
+  assert_select "a[href=?]", logout_path
+  assert_select "a[href=?]", "#", text: "Account"
+  assert_select "a[href=?]", user_path(@user)
+  # assert_select "a[href=?]", edit_user_path(@user)
+    # Comment above outputs 1 failure during test:
+    # Expected at least 1 element matching "a[href="/users/126448972/edit"]", found 0..
   end
 end
